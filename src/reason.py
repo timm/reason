d3cd10da7c3d314c5136906040a3a1e0044b39db
Tim Menzies <timm@ieee.org>
2021-09-18 01:34:39 -0400

    save

--- src/reason.py
+++ src/reason.py
@@ -1,22 +1,11 @@
 import re, random
 
+from functools import cmp_to_key as by 
+
 class o:
   def __init__(i, **d)  : i.__dict__.update(d)
   def __repr__(i) : return "{"+ ', '.join(
     [f":{k} {v}" for k, v in i.__dict__.items() if k[0] != "_"])+"}"
-
-def csv(file, sep=",", dull=r'([\n\t\r ]|#.*)'):
-  def atom(x):
-    if x=="?": return x
-    try: return float(x)
-    except Exception:  return x
-  with open(file) as fp:
-    for s in fp:
-      s=re.sub(dull,"",s)
-      if s: yield [atom(x) for x in s.split(sep)]
-
-def shuffle(lst): random.shuffle(lst); return lst
-
 class Col(o):
   def __init__(i,at,s): 
     i.n,i.at,i.name = 0,at,s
@@ -24,13 +13,12 @@ class Col(o):
 
   def add(i,x,n=1):
     if "?" != x:
-      i.n +=  1
-      x = i.add1(x,n)
+      i.n += 1; x = i.add1(x,n)
     return x
 
   def add1(i,x,n): return x
 
-  def dist(i,x,y): return 0 if x==y=="?" else i.dist1(x,y)
+  def dist(i,x,y): return 1 if x==y=="?" else i.dist1(x,y)
 
 class Num(Col):
   def __init__(i,*l,**kw): 
@@ -41,18 +29,22 @@ class Num(Col):
     x = float(x)
     if x > i.hi: i.hi = x
     if x < i.lo: i.lo = x
+    return x
 
-  def dist1(i,j,k):
-    if   x=="?": y= i.norm(y); x= 0 if x > .5 else 1
-    elif y=="?": x= i.norm(x); y= 0 if y > .5 else 1
-    else       : x,y = i.norm(x), i.norm(y)
-    return abs(x -  y)
+  def dist1(i,x,y):
+    if x=="?": 
+      y= i.norm(y); x= 0 if y > .5 else 1
+    elif y=="?": 
+      x= i.norm(x); y= 0 if x > .5 else 1
+    else:
+      x,y = i.norm(x), i.norm(y)
+    return abs(x - y)
 
   def norm(i,x):
     return 0 if abs(i.hi - i.lo) < 1E-31 else (x - i.lo)/(i.hi - i.lo)
 
 class Sym(Col):
-  def dist(i,x,y): return 0 if x==y else 1
+  def dist1(i,x,y): return 0 if x==y else 1
 
 class Sample(o):
   def __init__(i,inits=[]): 
@@ -73,22 +65,40 @@ class Sample(o):
 class Row(o):
   def __init__(i, s,lst,): i._sample, i.cells = s, lst
   def dist(i,j):
-    d,cols = 1E-31,i._sample.x
+    d,cols = 1E-31, i._sample.x
     for col in cols:
       tmp = col.dist(i.cells[col.at], j.cells[col.at])
       d  += tmp**2
     return (d/len(cols))**(1/2)
+  def mid(i,j):
+    out = [None] * len(i.cells)
+    for col,x,y in zip(i._sample.all, i.rows, j.rows):
 
 def first(lst): return lst[0]
+def shuffle(lst): random.shuffle(lst); return lst
+
+def csv(file, sep=",", dull=r'([\n\t\r ]|#.*)'):
+  def atom(x):
+    if x=="?": return x
+    try: return float(x)
+    except Exception:  return x
+  with open(file) as fp:
+    for s in fp:
+      s = re.sub(dull,"",s)
+      if s: yield [atom(x) for x in s.split(sep)]
+
+def gac(rows):
+  row1,*rows = rows
+  tmp = sorted(rows,key=lambda row2:row1.dist(row2))[2]
+  row1,row2, tmp[:2]
 
 random.seed(1)
 s=Sample()
-for row in csv("../data/weather.csv"):
+for row in csv("../data/auto93.csv"):
   s.add(row)
-s.rows = shuffle(s.rows)[:10]:
-for m,row1 in enumerate(s.rows):
-    (s.dist(row1,row2),row2) for row2 in s.rows)
-  
-a,b,c,d,e,f,g,h,*_ = shuffle(s.rows)
-print(c)
-  
+for row in shuffle(s.rows)[:10]:
+  tmp = sorted(s.rows, key=lambda z: row.dist(z))
+  print("")
+  print(row.cells)
+  print(tmp[1].cells, row.dist(tmp[1]))
+  print(tmp[-1].cells,row.dist(tmp[-1]))
